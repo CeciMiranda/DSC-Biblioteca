6fecced91fe2f09664b3b7ae8d80b0379641f515
Cecília Miranda <ceci.miranda254@gmail.com>
2023-11-01 07:41:00 -0300

    first commit

---
+++ biblioteca/src/test/java/br/edu/ifrn/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
